4f56efa108241b8a397751dce0666819d7d1ede6
DESKTOP-TQ503KN\User <ramya02107@gmail.com>
2022-01-06 12:00:20 +0530

    Two filesare addes

---
+++ CreateAPI.java
@@ -0,0 +1,11 @@
+class CreateAPI{
+public static void main(String[] args){
+system.out.println("hello World!");
+}
+public static void main(){
+system.out.println("hello world!");
+}
+public int add(int a,int b){
+return a+b;
+}
+}
